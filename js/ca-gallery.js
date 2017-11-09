49893115077bd36da04092dea3bcaf3c8dd33d75
ofir mossinson <ofirmoss@gmail.com>
2017-11-09 11:27:38 +0200

    add book shop link

--- js/ca-gallery.js
+++ js/ca-gallery.js
@@ -41,7 +41,7 @@ var gProjs = [{
     "id": "book-shop",
     "name": "book-shop",
     "title": "Better push those boxes",
-    "link": 'https://ofirmoss.github.io/book-shop/index.html',
+    "link": "https://ofirmoss.github.io/book-shop/index.html",
     "desc": "lorem ipsum lorem ipsum lorem ipsum",
     "publishedAt": 1448693940000,
     "labels": ["Matrixes", "keyboard events"],
